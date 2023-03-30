f531db0e019ecaa76c048b5026fd2bde4c4482d6
sirohm <junyantan95@gmail.com>
2023-03-30 15:49:59 +0800

    ans for JSON

---
+++ new project/week2_lecture.js
@@ -0,0 +1,44 @@
+// exercise 1//
+/*let message = "benr s"
+let number = 3
+console.log (message,number, "is my section")*/
+
+// exercise 2//
+/*
+let str = "25.5"
+let num = 3523
+str = parseInt (str)
+let course = parseInt(str) + num
+console.log (course)
+console.log (typeof str)
+let num2 = str  //suppose it stores 25 
+
+course = parseFloat(num2) + parseFloat (num)
+console.log (course)
+
+//exercise 3
+/*
+let data = ["benr", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+*/
+
+/*let user = {
+    name: "TAN",
+    age: 50,
+    faculty: "FKEKK" 
+}
+
+console.log (user.name)
+console.log (user.faculty)
+*/
+
+let messages = [
+    {name: "Ohm", msg: "nice", date:"4/4/2023", like: 1000}, 
+    {name: "jin", msg: "not bad", date:"5/4/2023", like: 200}
+]
+
+console.log (messages[1].msg)
